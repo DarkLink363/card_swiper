6f5f4f0576250cca7935358480a48f7807081dbf
Ariel Łowczyński <ariel363@o2.pl>
2021-09-30 12:09:27 +0200

    Update transformer_page_view.dart

--- lib/src/transformer_page_view/transformer_page_view.dart
+++ lib/src/transformer_page_view/transformer_page_view.dart
@@ -394,9 +394,6 @@ class _TransformerPageViewState extends State<TransformerPageView> {
   }
 
   double? _calcCurrentPixels() {
-    if (!_pageController!.hasClients) {
-      return 0;
-    }
     _currentPixels =
         _pageController!.getRenderIndexFromRealIndex(_activeIndex)! *
             _pageController!.position.viewportDimension *
